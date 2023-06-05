953d62e42fd9acd5e0c6b23443e1f2512da61e4c
unknown <nath.abhijit1990@gmail.com>
2023-06-06 03:28:56 +0530

    working code

--- src/components/GetBidDetails.js
+++ src/components/GetBidDetails.js
@@ -1,17 +1,19 @@
-import axios from "axios";
-import React, { Component } from "react";
-import ProductDetails from "./ProductDetails";
-import BidDetails from "./BidDetails";
+    import React, { Component } from "react";
+    import ProductDetails from "./ProductDetails";
+    import BidDetails from "./BidDetails";
+    import axios from "axios";
 
-class GetBidDetails extends Component {
+    class GetBidDetails extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            productId: '',
-            bidRows: []
+        constructor(props) {
+            super(props)
+            this.state = {
+                productId: '',
+                bidRows: [],
+                isInEditMode: false,
+                bidAmt: 0
+            }
         }
-    }
 
     handleProductIdChange = event => {
         this.setState({
@@ -33,56 +35,88 @@ class GetBidDetails extends Component {
              })
     }
 
-    
+    editBidAmount = (id) => {
+        this.setState({
+            isInEditMode: !this.state.isInEditMode
+        })
+        console.log(id);
+    }
+
+    handleBidAmountChange = (e, id) => {
+        let bidRows1 = [...this.state.bidRows];
+        bidRows1[id]['bid'].bidAmount = e.target.value;
+        this.setState({
+            bidRows: bidRows1
+        })
+
+    }
+
+    updateAmount = (e, id) => {
+            /*const formData = {
+                bidAmount: this.state.bidRows[id]["bid"].bidAmount,
+            }*/
+            let prodId = this.state.productId;
+            let email = this.state.bidRows[id].bid.email;
+            let updatedBidAmount = this.state.bidRows[id].bid.bidAmount;
+            const url = `http://localhost:8083/e-auction/api/v1/buyer/update-bid/${prodId}/${email}/${updatedBidAmount}`;
+            console.log(url);
+            axios.put(url)
+                 .then((response) => {
+                    console.log(response);
+                  })
+                 .catch((error) => {
+                    console.log(error);
+                  });
+    }
+
     render() {
-        var bidJA = [];
         const { bidRows } = this.state;
-        let productExist = bidRows.length > 0 ? true: false;
-        if(productExist) {
-            bidRows.map(row => bidJA.push(row.bid))
-        }
         //console.log('Value of bidJA is '+JSON.stringify(bidJA));
         return(
-            <div>
-                <br></br><br></br>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label id="productIdLabel">Product ID </label>&nbsp;
-                        <input type="text" value={this.state.productId} onChange={this.handleProductIdChange}/>&nbsp;&nbsp;
-                        <button type="submit">Get All Bids</button>
-                    </div>
-                </form>
-                <br></br>
-                {
-                    bidRows.length ? <ProductDetails {...bidRows[0].product} /> : null
-                }
-                <br></br><br></br>
-                {
-                    bidRows.length ? 
-                    <table id="tbl">
-                        <tbody>
-                            <tr>
-                                <th>Bid Amount</th>
-                                <th>Name </th>
-                                <th>Email </th>
-                                <th>Phone </th>
-                            </tr>
-                            {bidJA.map((item, index) => (
-                            <tr key={index}>
-                                <td><input type="text" value={item.bidAmount}/></td>
-                                <td>{item.firstName} {item.lastName}</td>
-                                <td>{item.email}</td>
-                                <td>{item.phone}</td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table> : null
-                    
-                }
-                
-            </div>
-        )
-    }
-}
+                <div>
+                    <br></br><br></br>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <label id="productIdLabel">Product ID </label>&nbsp;
+                            <input type="text" value={this.state.productId} onChange={this.handleProductIdChange}/>&nbsp;&nbsp;
+                            <button type="submit">Get All Bids</button>
+                        </div>
+                    </form>
+                    <br></br>
+                    {
+                        bidRows.length ? <ProductDetails {...bidRows[0].product} /> : null
+                    }
+                    <br></br><br></br>
+                        {
+                            bidRows.length ?
+                                <table id="tbl">
+                                    <tbody>
+                                        <tr>
+                                            <th>Bid Amount</th>
+                                            <th>Name </th>
+                                            <th>Email </th>
+                                            <th>Phone </th>
+                                        </tr>
+                                        {bidRows.map((row, rowIndex) => (
+                                        <tr key={rowIndex}>
+                                            <td><input type="text"
+                                                    value={row.bid.bidAmount}
+                                                    onChange={(e) => this.handleBidAmountChange(e,rowIndex)}/></td>
+                                            <td>{row.bid.firstName} {row.bid.lastName}</td>
+                                            <td>{row.bid.email}</td>
+                                            <td>{row.bid.phone}</td>
+                                            <td><button onClick={this.editBidAmount}>Edit</button>&nbsp;<button onClick={(e) => this.updateAmount(e, rowIndex)}>Update</button></td>
+                                        </tr>
+                                        ))}
+                                    </tbody>
+                            </table> : null
 
-export default GetBidDetails;
+                        }
+
+                </div>
+            )
+        }
+    }
+
+
+    export default GetBidDetails;
